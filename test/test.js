c4b0bd25803f718f3e52ee5ca4b58f0f46c12d89
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-03-16 11:17:49 -0700

    re-work tests per feedback

--- test/test.js
+++ test/test.js
@@ -17,59 +17,39 @@ describe('Test Router',() => {
 
   })
 
-  it('should test the GET routes', (done) => {
+  it('should test the GET routes and method', (done) => {
       notesRouter.routes.GET['/notes'];
       expect(notesRouter.routes).to.be.an('object')
+      expect(notesRouter.get).to.be.a('function');
       done();
   })
 
-  it('should test the PUT routes', (done) => {
+  it('should test the PUT routes and method', (done) => {
       notesRouter.routes.PUT['/notes'];
       expect(notesRouter.routes).to.be.an('object')
+      expect(notesRouter.put).to.be.a('function');
       done();
   })
 
-  it('should test the POST routes', (done) => {
+  it('should test the POST routes and method', (done) => {
       notesRouter.routes.POST['/notes'];
       expect(notesRouter.routes).to.be.an('object')
+      expect(notesRouter.post).to.be.a('function');
       done();
   })
 
-  it('should test the PATCH routes', (done) => {
+  it('should test the PATCH routes and method', (done) => {
       notesRouter.routes.PATCH['/notes'];
       expect(notesRouter.routes).to.be.an('object')
+      expect(notesRouter.patch).to.be.a('function');
       done();
   })
 
-  it('should test the DELETE routes', (done) => {
+  it('should test the DELETE routes and method', (done) => {
       notesRouter.routes.DELETE['/notes'];
       expect(notesRouter.routes).to.be.an('object')
+      expect(notesRouter.delete).to.be.a('function');
       done();
   })
 
-  it('should test that the GET method is a function', (done) => {
-    expect(notesRouter.get).to.be.a('function');
-    done();
-  });
-
-
-  it('should test that the POST method is a function', (done) => {
-    expect(notesRouter.post).to.be.a('function');
-    done();
-  });
-
-  it('should test that the PUT method is a function', (done) => {
-    expect(notesRouter.put).to.be.a('function');
-    done();
-  });
-
-  it('should test that the PATCH method is a function', (done) => {
-    expect(notesRouter.patch).to.be.a('function');
-    done();
-  });
-
-  it('should test that the DELETE method is a function', (done) => {
-    expect(notesRouter.delete).to.be.a('function');
-    done();
-  });
 })
